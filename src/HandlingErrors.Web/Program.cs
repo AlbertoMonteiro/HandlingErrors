7ebf89595a0efda3fc2109007bad7959bda16d27
Alberto Monteiro <alberto.monteiro@live.com>
2022-01-24 17:24:42 -0300

    Using top level statements

--- src/HandlingErrors.Web/Program.cs
+++ src/HandlingErrors.Web/Program.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using HandlingErrors.Web;
 
-namespace HandlingErrors.Web
-{
-    public class Program
-    {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
-    }
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()) 
+    .Build()
+    .Run();
